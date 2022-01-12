006446e76679070d8b3384faada2c0ed04e98db7
penny <fangle0121@gmail.com>
2022-01-11 16:58:47 -0800

    django_heroku

--- movie_trivia_service/settings.py
+++ movie_trivia_service/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('movie_api_endpoints_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['movieapidjangoapp.herokuapp.com']
 
 
 # Application definition
